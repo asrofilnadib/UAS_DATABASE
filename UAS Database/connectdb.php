cfd97c12c636bb9487215817c0fb861cf7bdab2c
asrofilnadib <60963555+asrofilnadib@users.noreply.github.com>
2023-02-05 12:50:52 +0700

    Delete UAS Database directory

--- UAS Database/connectdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	$conn = mysqli_connect('localhost', 'root',
-	'', 'rukun_tetangga');
-	
-	if (mysqli_connect_errno()) {
-		echo "Koneksi ke server gagal" . mysqli_connect_error();
-		exit();
-	}
